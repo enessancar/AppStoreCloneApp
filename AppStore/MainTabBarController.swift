b6889aa012b8d9ee3a3b39d498b728a6a6ba88c5
enessancar <enes57751907@gmail.com>
2023-04-27 00:42:46 +0300

    Initial Commit

---
+++ AppStore/MainTabBarController.swift
@@ -0,0 +1,17 @@
+//
+//  MainTabBarController.swift
+//  AppStore
+//
+//  Created by Enes Sancar on 27.04.2023.
+//
+
+import UIKit
+
+class MainTabBarController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
---
+++ AppStore/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AppStore
+//
+//  Created by Enes Sancar on 27.04.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .gray
+    }
+
+
+}
+
