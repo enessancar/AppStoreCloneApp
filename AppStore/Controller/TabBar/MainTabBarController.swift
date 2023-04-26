d932d325dd7faece818c224bbdf5a196be3f82c7
enessancar <enes57751907@gmail.com>
2023-04-27 01:21:45 +0300

    folder layout has been created  the project is created programmatically,  tab bar has been added

---
+++ AppStore/Controller/TabBar/MainTabBarController.swift
@@ -0,0 +1,36 @@
+//
+//  MainTabBarController.swift
+//  AppStore
+//
+//  Created by Enes Sancar on 27.04.2023.
+//
+
+import UIKit
+
+class MainTabBarController: UITabBarController {
+
+    //MARK: - Lifecycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupVC()
+    }
+    
+    private func setupVC() {
+        viewControllers = [
+            createNavController(viewController: UIViewController(), title: "Today", imageName: "doc.text.image"),
+            createNavController(viewController: UIViewController(), title: "Apps", imageName: "square.stack.3d.up.fill"),
+            createNavController(viewController: SearchViewController(), title: "Search", imageName: "magnifyingglass")
+        ]
+    }
+    
+    fileprivate func createNavController(viewController: UIViewController, title: String, imageName: String) -> UIViewController {
+        
+        let navController = UINavigationController(rootViewController: viewController)
+        navController.navigationBar.prefersLargeTitles = true
+        viewController.navigationItem.title = title
+        viewController.view.backgroundColor = .systemBackground
+        navController.tabBarItem.title = title
+        navController.tabBarItem.image = UIImage(systemName: imageName)
+        return navController
+    }
+}
---
+++ AppStore/SearchViewController.swift
@@ -0,0 +1,39 @@
+//
+//  ViewController.swift
+//  AppStore
+//
+//  Created by Enes Sancar on 27.04.2023.
+//
+
+import UIKit
+
+class SearchViewController: UICollectionViewController {
+    
+    //MARK: - Lifecycle
+    init() {
+        super.init(collectionViewLayout: UICollectionViewFlowLayout())
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
+//MARK: - Helpers
+extension SearchViewController {
+    
+    private func style() {
+        
+    }
+    
+    private func layout() {
+        
+    }
+}
+
+
+
